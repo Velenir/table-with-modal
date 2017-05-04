3c189f8037d34d1c1bfd56f24333abc097dc913a
Velenir <Velenir@users.noreply.github.com>
2017-05-04 16:09:23 +0300

    feat(components): Scaffold TableControls component

---
+++ app/components/TableControls.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+ 
+export default () => (
+	<div className="table-controls">
+		<a className="table-controls__button table-controls__button--save">Сохранить расчеты</a>
+		<a className="table-controls__button table-controls__button--print">Распечатать</a>
+	</div>
+);
