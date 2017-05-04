77deff6697569b9d08edbbb3ba1ffff40d0a0506
Velenir <Velenir@users.noreply.github.com>
2017-05-04 16:08:51 +0300

    feat(components): Scaffold TableTitle component

---
+++ app/components/TableTitle.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+ 
+export default () => (
+	<div className="table-title">
+		<p className="table-title__subtitle">Оптимизация кредита: по сумме</p>
+		<h2 className="table-title__title">Расчет платежей</h2>
+	</div>
+);
