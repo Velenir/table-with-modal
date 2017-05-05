824341663834d72467ea3d6f6008e29fec8e00fc
Velenir <Velenir@users.noreply.github.com>
2017-05-05 11:57:41 +0300

    feat(style): Style table for the most part

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -7,12 +7,13 @@ export default class Table extends Component {
 		
 		return (
 			<table className="table-contents">
-				<thead>
+				<thead className="table-contents__head">
 					<tr>
 						{head.map((title, i) => <th key={i}>{title}</th>)}
+						<th></th>
 					</tr>
 				</thead>
-				<tbody>
+				<tbody className="table-contents__body">
 					{rows.map((row, i) => <TableRow key={i} row={row} ind={i} currency={currency}/>)}
 				</tbody>
 			</table>
