63e548ef4be12cadeffd5040c244e6edfed6b4e5
Velenir <Velenir@users.noreply.github.com>
2017-05-05 12:07:10 +0300

    refactor(style): Change title size

--- app/components/TableTitle.jsx
+++ app/components/TableTitle.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default () => (
 	<div className="table-title">
 		<p className="table-title__subtitle">Оптимизация кредита: по сумме</p>
-		<h2 className="table-title__title">Расчет платежей</h2>
+		<h1 className="table-title__title">Расчет платежей</h1>
 	</div>
 );
