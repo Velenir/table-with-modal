20c5ec5e0da637e9c96ee1217493ac0950a7a290
Velenir <Velenir@users.noreply.github.com>
2017-05-05 20:14:44 +0300

    refactor(style): Style CloseButton

--- app/components/AddedPayment.jsx
+++ app/components/AddedPayment.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import "../styles/addedPayment.scss";
+import CloseButton from "../images/closeButton.svg";
  
 const AddedPayment = ({value, saving, currency}) => (
 	<div className="added-payment">
-		<a className="close-button added-payment__button" href="#">X</a>
+		<a className="close-button added-payment__button" href="#"><CloseButton height="100%" width={undefined}/></a>
 		<div>
 			<p className="added-payment__value">{value} <span className="rouble">{currency}</span></p>
 			<p className="added-payment__saving">{saving} <span className="rouble">{currency}</span></p>
