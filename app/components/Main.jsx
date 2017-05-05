57b21f9303c88de3ebabd96ff722faf80f19647c
Velenir <Velenir@users.noreply.github.com>
2017-05-05 11:25:07 +0300

    refactor(state): Pass table values through props

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,22 @@ import TableTitle from "./TableTitle";
 import Table from "./Table";
 import TableControls from "./TableControls";
 
+import "../styles/main.scss";
+
+const tableData = {
+	head: ["№", "Дата", "Сумма переплат", "Остаток задолженности", "Основной долг", "Начисленные проценты", "Платеж"],
+	rows: [
+		["05.16", "30 000 ₽", "3 450 000 ₽", "10 000 ₽", "30 000 ₽", "40 000 ₽"],
+		["06.16", "60 000 ₽", "3 440 000 ₽", "10 000 ₽", "30 000 ₽", "40 000 ₽"],
+		["07.16", "90 000 ₽", "3 430 000 ₽", "10 000 ₽", "30 000 ₽", "40 000 ₽"],
+		["08.16", "120 000 ₽", "3 420 000 ₽", "10 000 ₽", "30 000 ₽", "40 000 ₽"],
+	]
+};
+
 const Main = () => (
 	<div className="container">
 		<TableTitle/>
-		<Table/>
+		<Table {...tableData}/>
 		<TableControls/>
 	</div>
 );
--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
-
-const headCells = ["No", "Дата", "Сумма переплат", "Остаток задолженности", "Основной долг", "Начисленные проценты", "Платеж"];
+import TableRow from "./TableRow";
 
 export default class Table extends Component {
 	render() {
+		const {head, rows} = this.props;
+		
 		return (
-			<table>
+			<table className="table-contents">
 				<thead>
 					<tr>
-						{headCells.map((title, i) => <th key={i}>{title}</th>)}
+						{head.map((title, i) => <th key={i}>{title}</th>)}
 					</tr>
 				</thead>
+				<tbody>
+					{rows.map((val, i) => <TableRow key={i} values={val} ind={i}/>)}
+				</tbody>
 			</table>
 		);
 	}
