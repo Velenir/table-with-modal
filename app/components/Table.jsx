646f673c72f5338c241f2feaf0863d03d973b465
Velenir <Velenir@users.noreply.github.com>
2017-05-05 11:18:56 +0300

    feat(components): Display head row

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 
+const headCells = ["No", "Дата", "Сумма переплат", "Остаток задолженности", "Основной долг", "Начисленные проценты", "Платеж"];
+
 export default class Table extends Component {
 	render() {
 		return (
-			<table></table>
+			<table>
+				<thead>
+					<tr>
+						{headCells.map((title, i) => <th key={i}>{title}</th>)}
+					</tr>
+				</thead>
+			</table>
 		);
 	}
 }
