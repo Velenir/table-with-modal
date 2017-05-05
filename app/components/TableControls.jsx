084624281734c0fbaa0b61a4913a2e3ac296329b
Velenir <Velenir@users.noreply.github.com>
2017-05-05 12:04:37 +0300

    feat(buttons): Style buttons

--- app/components/TableControls.jsx
+++ app/components/TableControls.jsx
@@ -2,7 +2,7 @@ import React from 'react';
  
 export default () => (
 	<div className="table-controls">
-		<a className="table-controls__button table-controls__button--save">Сохранить расчеты</a>
-		<a className="table-controls__button table-controls__button--print">Распечатать</a>
+		<a className="table-controls__button table-controls__button--save" href="#">Сохранить расчеты</a>
+		<a className="table-controls__button table-controls__button--print" href="#">Распечатать</a>
 	</div>
 );
