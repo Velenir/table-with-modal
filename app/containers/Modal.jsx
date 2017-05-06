d834a93057583b18f09fa00a59c878807f4fd605
Velenir <Velenir@users.noreply.github.com>
2017-05-06 17:46:50 +0300

    refactor(containers): Simplify passing modal props

--- app/containers/Modal.jsx
+++ app/containers/Modal.jsx
@@ -4,6 +4,6 @@ import {closeModal} from '../actions';
 import Modal from '../components/Modal';
 
 
-const mapStateToProps = ({modal}) => ({open: modal && modal.open});
+const mapStateToProps = ({modal}) => ({open: modal.open});
 
 export default connect(mapStateToProps, {closeModal})(Modal);
