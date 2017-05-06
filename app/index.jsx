3b2109a1b6144c3c369c0c6efbceb890da33303c
Velenir <Velenir@users.noreply.github.com>
2017-05-06 11:10:34 +0300

    feat(store): Wrapp main app in store provider

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import {Provider} from 'react-redux';
+import {createMyStore} from './helpers';
+
 import Main from './components/Main';
 
+const store = createMyStore();
+
 render(
-	<Main/>,
+	<Provider store={store}>
+		<Main/>
+	</Provider>,
 	document.getElementById("app")
 );
