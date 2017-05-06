72c16d479f9468318d9f2e5f85c6e98beb2e4c63
Velenir <Velenir@users.noreply.github.com>
2017-05-06 16:48:39 +0300

    feat(store): Abstract away AddPaymentDialog and connect it to store

---
+++ app/components/AddPaymentDialog.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ControlButton from "./ControlButton";
+
+class AddPaymentDialog extends Component {
+	onSaveClicked = () => {
+		const {rowInd, saveAndCloseModal} = this.props;
+		saveAndCloseModal(rowInd, this.input.value);
+	}
+	
+	render() {
+		return (
+			<div>
+				<h3 className="modal__title">Сумма платежа</h3>
+				<input type="text" className="modal__input" defaultValue="0 a" ref={c => this.input = c}/>
+				<div className="modal__controls">
+					<ControlButton className="modal__controls__button" onClick={this.onSaveClicked}>
+						Сохранить
+					</ControlButton>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default AddPaymentDialog;
---
+++ app/containers/AddPaymentDialog.jsx
@@ -0,0 +1,9 @@
+import {connect} from 'react-redux';
+import {saveAndCloseModal} from '../actions';
+
+import AddPaymentDialog from '../components/AddPaymentDialog';
+
+
+const mapStateToProps = ({modal: {rowInd}}) => ({rowInd});
+
+export default connect(mapStateToProps, {saveAndCloseModal})(AddPaymentDialog);
