3ee747804fe6134a409209ae48fe452caa641c93
Velenir <Velenir@users.noreply.github.com>
2017-05-07 21:17:37 +0300

    chore: Cleanup

--- app/components/AddPaymentDialog.jsx
+++ app/components/AddPaymentDialog.jsx
@@ -15,7 +15,6 @@ class AddPaymentDialog extends Component {
 	
 	onInputChange = () => {
 		const inputInvalid = !this.input.checkValidity();
-		console.log(!inputInvalid);
 		
 		if(this.state.buttonDisabled !== inputInvalid) {
 			this.setState({
@@ -28,7 +27,6 @@ class AddPaymentDialog extends Component {
 		return (
 			<div>
 				<h3 className="modal__title">Сумма платежа</h3>
-				<input type="text" className="modal__input" defaultValue="0 a" ref={c => this.input = c}/>
 				<MoneyInput className="modal__input" ref={c => this.input = c} onChange={this.onInputChange} currency={this.props.currency}/>
 				<div className="modal__controls">
 					<ControlButton className="modal__controls__button" onClick={this.onSaveClicked} disabled={this.state.buttonDisabled}>
